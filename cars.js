e9a11c7e234b2f9fc6f768cfa2b02de0ca040970
Suha-AlHumaid <Suha.Saleh.elq.js@tuwaiq.edu.sa>
2021-10-19 11:35:37 +0300

    task

--- cars.js
+++ cars.js
@@ -8,16 +8,18 @@ constructor() {
     this.colour=colour, 
     this.image=image, 
     this.registrationNumber=registrationNumber,
-    this.price=price;
-     function editPrice(newPrice) {
+    this.price=price;}
+    editPrice(newPrice) {
 this.price = newPrice*5;
      }
-     function showAll() {
+     showAll() {
 return ` Make Date ${this.make} ,Model ${this.model}, Colour ${this.colour}, Image ${this.image}, Registration Number${this.registrationNumber} ,price of 2021 ${this.newPrice(this.price)}`;
      }
+
 };
 
-}
+let bmw = class cars("2019","BMW","Red", "url", "3928203" ,"30000");
+cars.showAll();
 //Each car object should also include a editPrice(newPrice) method.
 // this method should change the price of the car
 
