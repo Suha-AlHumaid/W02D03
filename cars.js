dc825feeebbb32afa23afe234c5f163e63ec271f
Suha-AlHumaid <Suha.Saleh.elq.js@tuwaiq.edu.sa>
2021-10-19 11:28:34 +0300

    task

---
+++ cars.js
@@ -0,0 +1,24 @@
+// constract function
+class cars {
+
+
+constructor() {
+    this.make= make, 
+    this.model= model, 
+    this.colour=colour, 
+    this.image=image, 
+    this.registrationNumber=registrationNumber,
+    this.price=price;
+     function editPrice(newPrice) {
+this.price = newPrice*5;
+     }
+     function showAll() {
+return ` Make Date ${this.make} ,Model ${this.model}, Colour ${this.colour}, Image ${this.image}, Registration Number${this.registrationNumber} ,price of 2021 ${this.newPrice(this.price)}`;
+     }
+};
+
+}
+//Each car object should also include a editPrice(newPrice) method.
+// this method should change the price of the car
+
+
